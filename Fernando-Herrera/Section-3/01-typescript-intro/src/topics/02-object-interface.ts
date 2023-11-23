24f1a0d2fe6ab6f0fe9104f60f59f1cba0883f5b
ingaamira <andres.mira@outlook.com>
2023-11-23 12:43:40 -0500

    Add object, array and interface

---
+++ Fernando-Herrera/Section-3/01-typescript-intro/src/topics/02-object-interface.ts
@@ -0,0 +1,25 @@
+
+/**Arreglos de String */
+//const skills: string[] = ['Bash','Counter','Healing']; 
+
+/**Creando Interfaces:
+ * Permite indicar a los obejetos cómo pueden lucir o qué caracteristicas pueden tener
+ */
+interface Character {
+    name: String;
+    hp: number;
+    skills: string[];
+    hometown?: string; /**Opcional */
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 100,
+    skills: ['Bash', 'Counter'],
+};
+
+strider.hometown = 'Rivendell';
+
+console.table(strider);
+
+export{};
