82f21929eeb4d8814f678aae55cb662cae96ef43
ingaamira <andres.mira@outlook.com>
2023-11-27 08:26:50 -0500

    Add counter

--- Fernando-Herrera/Section-4/bases/src/app/app.module.ts
+++ Fernando-Herrera/Section-4/bases/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CounterModule,
     AppRoutingModule
   ],
   providers: [
